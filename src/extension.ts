dd008198522f25e1527e2857436a13efc5b9729b
Thomas Austin <thomasjaustin@icloud.com>
2019-11-01 10:50:58 +1100

    Extension improvements and code optimisations

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
-import * as vscode from 'vscode'
+import { window, ExtensionContext, commands, StatusBarAlignment, workspace } from 'vscode'
 import * as fs from 'fs'
 import * as fm from 'front-matter'
 import * as path from 'path'
 import * as sendgrid from '@sendgrid/mail'
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
         let commandList = [
                 {
                         label: 'Send Email',
@@ -30,30 +30,32 @@ export function activate(context: vscode.ExtensionContext) {
                         label: 'Delete Config',
                         description: 'Deletes a config from the saved list',
                         command: 'kirim.deleteconfig'
+                },
+                {
+                        label: 'Save Key',
+                        description: 'Saves the SendGrid API key in extension settings',
+                        command: 'kirim.savekey'
                 }
         ]
 
-        const showCommands = () => {
-                async function showQuickPick() {
-                        let commandName = await vscode.window.showQuickPick(commandList, {
-                                placeHolder: 'Please select a command to run...'
-                        })
-                        if (!commandName) {
-                                return
-                        }
-                        vscode.commands.executeCommand(commandName.command)
+        const showCommands = async () => {
+                let commandName = await window.showQuickPick(commandList, {
+                        placeHolder: 'Select a command to run...'
+                })
+                if (!commandName) {
+                        return
                 }
-                showQuickPick()
+                commands.executeCommand(commandName.command)
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.showcommands', showCommands))
+        context.subscriptions.push(commands.registerCommand('kirim.showcommands', showCommands))
 
-        let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0)
+        let statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 0)
         statusBarItem.text = '$(mail) Kirim'
         statusBarItem.command = 'kirim.showcommands'
 
         const updateStatusBarItem = () => {
-                let activeEditor = vscode.window.activeTextEditor
+                let activeEditor = window.activeTextEditor
                 if (!activeEditor || activeEditor.document.languageId !== 'html') {
                         statusBarItem.hide()
                         return
@@ -62,25 +64,42 @@ export function activate(context: vscode.ExtensionContext) {
         }
         updateStatusBarItem()
 
-        context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem))
+        context.subscriptions.push(window.onDidChangeActiveTextEditor(updateStatusBarItem))
 
         const sendEmail = () => {
-                let activeEditor = vscode.window.activeTextEditor
+                let activeEditor = window.activeTextEditor
                 if (!activeEditor || activeEditor.document.languageId !== 'html') {
-                        vscode.window.showErrorMessage('Please open a HTML file before running this command.')
+                        window.showErrorMessage('Open a HTML file before running this command.')
                         return
                 }
                 if (activeEditor.document.isDirty) {
-                        vscode.window.showErrorMessage('Please save your file before running this command.')
+                        window.showErrorMessage('Save your file before running this command.')
                         return
                 }
                 let editorPath = activeEditor.document.uri.fsPath
                 let editorData = fs.readFileSync(editorPath)
                 if (!fm.test(editorData.toString())) {
-                        vscode.window.showErrorMessage('Please load a config before sending.')
+                        window.showErrorMessage('Load a config before running this command.', 'Load Config').then(action => {
+                                switch (action) {
+                                        case 'Load Config':
+                                                loadConfig()
+                                                break
+                                }
+                        })
                         return
                 }
                 try {
+                        let apiKey: any = workspace.getConfiguration('kirim').get('authenticationToken')
+                        if (!apiKey) {
+                                window.showErrorMessage('Save your API key before running this command.', 'Save API Key').then(action => {
+                                        switch (action) {
+                                                case 'Save API Key':
+                                                        saveKey()
+                                                        break
+                                        }
+                                })
+                                return
+                        }
                         let editorDataExcerpt: any = fm(editorData.toString())
                         let emailData = {
                                 to: editorDataExcerpt.attributes['to'],
@@ -88,55 +107,55 @@ export function activate(context: vscode.ExtensionContext) {
                                 subject: editorDataExcerpt.attributes['subject'],
                                 html: editorDataExcerpt.body
                         }
-                        sendgrid.setApiKey(editorDataExcerpt.attributes['key'])
+                        sendgrid.setApiKey(apiKey)
                         sendgrid.sendMultiple(emailData)
                                 .then(() => {
-                                        vscode.window.showInformationMessage('Your email has been successfully sent.')
+                                        window.showInformationMessage('Your email has been successfully sent.')
                                 })
                                 .catch(error => {
-                                        vscode.window.showErrorMessage(error.toString())
+                                        window.showErrorMessage(error.toString())
                                 })
                 } catch (error) {
-                        vscode.window.showErrorMessage(error.toString())
+                        window.showErrorMessage(error.toString())
                 }
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.sendemail', sendEmail))
+        context.subscriptions.push(commands.registerCommand('kirim.sendemail', sendEmail))
 
         const clearConfig = () => {
-                let activeEditor = vscode.window.activeTextEditor
+                let activeEditor = window.activeTextEditor
                 if (!activeEditor || activeEditor.document.languageId !== 'html') {
-                        vscode.window.showErrorMessage('Please open a HTML file before running this command.')
+                        window.showErrorMessage('Open a HTML file before running this command.')
                         return
                 }
                 if (activeEditor.document.isDirty) {
-                        vscode.window.showErrorMessage('Please save your file before running this command.')
+                        window.showErrorMessage('Save your file before running this command.')
                         return
                 }
                 let editorPath = activeEditor.document.uri.fsPath
                 let editorData = fs.readFileSync(editorPath)
                 if (!fm.test(editorData.toString())) {
-                        vscode.window.showErrorMessage('There doesn’t seem to be a config to clear.')
+                        window.showErrorMessage('There doesn’t seem to be a config to clear.')
                         return
                 }
                 try {
                         let editorDataExcerpt = fm(editorData.toString()).body
                         fs.writeFileSync(editorPath, editorDataExcerpt)
                 } catch (error) {
-                        vscode.window.showErrorMessage(error.toString())
+                        window.showErrorMessage(error.toString())
                 }
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.clearconfig', clearConfig))
+        context.subscriptions.push(commands.registerCommand('kirim.clearconfig', clearConfig))
 
         const loadConfig = () => {
-                let activeEditor = vscode.window.activeTextEditor
+                let activeEditor = window.activeTextEditor
                 if (!activeEditor || activeEditor.document.languageId !== 'html') {
-                        vscode.window.showErrorMessage('Please open a HTML file before running this command.')
+                        window.showErrorMessage('Open a HTML file before running this command.')
                         return
                 }
                 if (activeEditor.document.isDirty) {
-                        vscode.window.showErrorMessage('Please save your file before running this command.')
+                        window.showErrorMessage('Save your file before running this command.')
                         return
                 }
                 let editorPath = activeEditor.document.uri.fsPath
@@ -147,15 +166,15 @@ export function activate(context: vscode.ExtensionContext) {
                         .filter(template => template.includes('thomasaustin.kirim'))
                         .map(template => template.replace(/thomasaustin.kirim.|.yaml/g, ''))
                 if (templateList.length <= 0) {
-                        let templateData = `---\nname: \nkey: \nto: \n  - \n  - \nfrom: \nsubject: \n---`
+                        let templateData = `---\nname: \nto: \n  - \n  - \nfrom: \nsubject: \n---`
                         let editorDataExcerpt = fm(editorData.toString()).body
                         let dataMerge = [templateData, editorDataExcerpt].join('\n')
                         fs.writeFileSync(editorPath, dataMerge)
                         return
                 }
                 async function showQuickPick() {
-                        let templateName = await vscode.window.showQuickPick(templateList, {
-                                placeHolder: 'Please select a saved config...'
+                        let templateName = await window.showQuickPick(templateList, {
+                                placeHolder: 'Select a saved config...'
                         })
                         if (!templateName) {
                                 return
@@ -168,41 +187,47 @@ export function activate(context: vscode.ExtensionContext) {
                 showQuickPick()
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.loadconfig', loadConfig))
+        context.subscriptions.push(commands.registerCommand('kirim.loadconfig', loadConfig))
 
         const saveConfig = () => {
-                let activeEditor = vscode.window.activeTextEditor
+                let activeEditor = window.activeTextEditor
                 if (!activeEditor || activeEditor.document.languageId !== 'html') {
-                        vscode.window.showErrorMessage('Please open a HTML file before running this command.')
+                        window.showErrorMessage('Open a HTML file before running this command.')
                         return
                 }
                 if (activeEditor.document.isDirty) {
-                        vscode.window.showErrorMessage('Please save your file before running this command.')
+                        window.showErrorMessage('Save your file before running this command.')
                         return
                 }
                 let editorPath = activeEditor.document.uri.fsPath
                 let editorData = fs.readFileSync(editorPath)
                 let templatePath = context.globalStoragePath
                 if (!fm.test(editorData.toString())) {
-                        vscode.window.showErrorMessage('Please load a config before saving.')
+                        window.showErrorMessage('Load a config before running this command.', 'Load Config').then(action => {
+                                switch (action) {
+                                        case 'Load Config':
+                                                loadConfig()
+                                                break
+                                }
+                        })
                         return
                 }
                 try {
                         let editorDataExcerpt: any = fm(editorData.toString())
                         if (editorDataExcerpt.attributes['name'] === null) {
-                                vscode.window.showErrorMessage('Please specify a config name before saving.')
+                                window.showErrorMessage('Specify a config name before running this command.')
                                 return
                         }
                         let templateData = '---\n' + editorDataExcerpt.frontmatter + '\n---'
                         let templateFilePath = templatePath + '.' + editorDataExcerpt.attributes['name'] + '.yaml'
                         fs.writeFileSync(templateFilePath, templateData)
-                        vscode.window.showInformationMessage(editorDataExcerpt.attributes['name'] + ' has been successfully saved.')
+                        window.showInformationMessage(`${editorDataExcerpt.attributes['name']} has been successfully saved.`)
                 } catch (error) {
-                        vscode.window.showErrorMessage(error.toString())
+                        window.showErrorMessage(error.toString())
                 }
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.saveconfig', saveConfig))
+        context.subscriptions.push(commands.registerCommand('kirim.saveconfig', saveConfig))
 
         const deleteConfig = () => {
                 let templatePath = context.globalStoragePath
@@ -211,24 +236,38 @@ export function activate(context: vscode.ExtensionContext) {
                         .filter(template => template.includes('thomasaustin.kirim'))
                         .map(template => template.replace(/thomasaustin.kirim.|.yaml/g, ''))
                 if (templateList.length <= 0) {
-                        vscode.window.showErrorMessage('There doesn’t seem to be a config to delete.')
+                        window.showErrorMessage('There doesn’t seem to be a config to delete.')
                         return
                 }
                 async function showQuickPick() {
-                        let templateName = await vscode.window.showQuickPick(templateList, {
-                                placeHolder: 'Please select a config to delete...'
+                        let templateName = await window.showQuickPick(templateList, {
+                                placeHolder: 'Select a config to delete...'
                         })
                         if (!templateName) {
                                 return
                         }
                         let templateFilePath = templatePath + '.' + templateName + '.yaml'
                         fs.unlinkSync(templateFilePath)
-                        vscode.window.showInformationMessage(templateName + ' has been successfully deleted.')
+                        window.showInformationMessage(`${templateName} has been successfully deleted.`)
                 }
                 showQuickPick()
         }
 
-        context.subscriptions.push(vscode.commands.registerCommand('kirim.deleteconfig', deleteConfig))
+        context.subscriptions.push(commands.registerCommand('kirim.deleteconfig', deleteConfig))
+
+        const saveKey = async () => {
+                let apiKey = await window.showInputBox({
+                        prompt: 'Paste your API key here'
+                })
+                if (!apiKey) {
+                        return
+                }
+                let config = workspace.getConfiguration('kirim')
+                config.update('authenticationToken', apiKey, true)
+                window.showInformationMessage('Your API key has been successfully saved.')
+        }
+
+        context.subscriptions.push(commands.registerCommand('kirim.savekey', saveKey))
 }
 
 export function deactivate() {}
